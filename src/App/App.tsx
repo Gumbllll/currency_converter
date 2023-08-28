9ccbbdffff862765fb7e34c8ab5c586bb54e79af
Gumbllll <wot_2009@bk.ru>
2023-08-28 13:31:23 +0300

    Настроил сервер, добавил состояния валюты

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import Block from '../componets/Block';
 import styles from './App.module.scss';
 
 function App() {
+
+  const [ fromCurrency, setFromCurrency ] = useState('RUB');
+  const [ toCurrency, setToCurrency ] = useState('USD');
+  const [ fromValue, setFromValue ] = useState('0');
+  const [ toValue, setToValue ] = useState('0'); 
+
+
   return (
     <div className={styles.App}>
       <div className={styles.AppMain}>
-        <Block/>
-        <Block/>
+        <Block currency={fromCurrency} setCurrency={setFromCurrency} value={fromValue} setValue={setFromValue} />
+        <Block currency={toCurrency} setCurrency={setToCurrency} value={toValue} setValue={setToValue}/>
       </div> 
     </div>
   );
--- src/componets/Block.tsx
+++ src/componets/Block.tsx
@@ -1,17 +1,26 @@
 import styles from './Block.module.scss'
 
-const Block = () => {
+interface ICurrency {
+    currency: string,
+    setCurrency: (item: string) => void,
+    value: string,
+    setValue: (value: string) => void,
+}
 
-    const currency = ['RUB', 'USD', 'EUR', 'JPY'];
+const Block: React.FC<ICurrency> = ({ currency, setCurrency, value, setValue }) => {
 
+    const currencyList = ['RUB', 'USD', 'EUR', 'JPY'];
+    
     return (
         <main className={styles.main}>
             <div className={styles.mainCurrency}>
-                {currency.map(item => <div className={styles.mainCurrencyItem}>{item}</div>)}
+                {currencyList.map((item) => <div onClick={() => setCurrency(item)} className={ item === currency ? styles.mainCurrencyItemActive : styles.mainCurrencyItem}>{item}</div>)}
             </div>
-            <input className={styles.mainInput} type="text" />
+            <input onChange={(e) => setValue(e.target.value)} value={value} className={styles.mainInput}/>
         </main>
     )
 }
+
+
 
 export default Block;
