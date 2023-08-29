587478a7979a9df3358dd9b522cefef303e4fc68
Gumbllll <wot_2009@bk.ru>
2023-08-29 22:04:17 +0300

    Добавли логику расчета валюты

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Block from '../componets/Block';
 import styles from './App.module.scss';
+import axios from 'axios';
+
+interface IRates {
+  [key: string]: number;
+}
 
 function App() {
 
   const [ fromCurrency, setFromCurrency ] = useState('RUB');
   const [ toCurrency, setToCurrency ] = useState('USD');
-  const [ fromValue, setFromValue ] = useState('0');
-  const [ toValue, setToValue ] = useState('0'); 
+  const [ fromValue, setFromValue ] = useState(0);
+  const [ toValue, setToValue ] = useState(0); 
+  const [rates, setRates] = useState<IRates>({});
 
+  console.log(fromValue);
+
+  useEffect(() => {
+    (async function() {
+      const response = await axios.get('http://localhost:3001/rates');
+      setRates(response.data);
+    })();
+  }, []);
+
+  console.log(rates[fromCurrency]);
+
+  const onChangeFromRates = (value: number) => {
+    const price = (value / rates[fromCurrency]) * rates[toCurrency];
+    setFromValue(value);
+    setToValue(+price.toFixed(6)); 
+  }
+
+  const onChangeToRates = (value: number) => {
+    const price = (rates[fromCurrency] / rates[toCurrency] * value);
+    setFromValue(+price.toFixed(6));
+    setToValue(value);
+  }
+
+  // useEffect(() => {
+  //   onChangeFromRates(fromValue);
+  // }, [toCurrency, toValue]);
+
+  // useEffect(() => {
+  //   onChangeToRates(toValue);
+  // }, [fromCurrency, fromValue])
 
   return (
     <div className={styles.App}>
       <div className={styles.AppMain}>
-        <Block currency={fromCurrency} setCurrency={setFromCurrency} value={fromValue} setValue={setFromValue} />
-        <Block currency={toCurrency} setCurrency={setToCurrency} value={toValue} setValue={setToValue}/>
+        <Block currency={fromCurrency} setCurrency={setFromCurrency} valueR={fromValue} setValue={onChangeFromRates} />
+        <Block currency={toCurrency} setCurrency={setToCurrency} valueR={toValue} setValue={onChangeToRates}/>
       </div> 
     </div>
   );
--- src/componets/Block.tsx
+++ src/componets/Block.tsx
@@ -3,20 +3,20 @@ import styles from './Block.module.scss'
 interface ICurrency {
     currency: string,
     setCurrency: (item: string) => void,
-    value: string,
-    setValue: (value: string) => void,
+    valueR: number,
+    setValue: (value: number) => void,
 }
 
-const Block: React.FC<ICurrency> = ({ currency, setCurrency, value, setValue }) => {
+const Block: React.FC<ICurrency> = ({ currency, setCurrency, valueR, setValue }) => {
 
     const currencyList = ['RUB', 'USD', 'EUR', 'JPY'];
     
     return (
         <main className={styles.main}>
             <div className={styles.mainCurrency}>
-                {currencyList.map((item) => <div onClick={() => setCurrency(item)} className={ item === currency ? styles.mainCurrencyItemActive : styles.mainCurrencyItem}>{item}</div>)}
+                {currencyList.map((item) => <div key={item} onClick={() => setCurrency(item)} className={ item === currency ? styles.mainCurrencyItemActive : styles.mainCurrencyItem}>{item}</div>)}
             </div>
-            <input onChange={(e) => setValue(e.target.value)} value={value} className={styles.mainInput}/>
+            <input onChange={(e) => setValue(Number(e.target.value))} value={valueR} className={styles.mainInput}/>
         </main>
     )
 }
